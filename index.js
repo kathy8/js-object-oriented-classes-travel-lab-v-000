a99f26436957ad555e8e74a26252b76124f2e882
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-04-14 20:31:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,3 +34,14 @@ class Route {
       this.beginningLocation = beginningLocation;
         this.endingLocation = endingLocation;
     }
+
+
+blocksTravelled() {
+    let startHorizontal = eastWest.indexOf(this.beginningLocation.horizontal);
+        let endHorizontal = eastWest.indexOf(this.endingLocation.horizontal);
+        return Math.abs(startHorizontal - endHorizontal) + Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical);
+    }
+    estimatedTime(peakHours = false) {
+        return peakHours ? this.blocksTravelled() / 2  : this.blocksTravelled() / 3 ;
+    }
+} 
